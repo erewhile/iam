e98dda42c27bbb14f0d9d51134e392472bf0cd2b
Eren <eren@xiazhi.net>
2026-06-21 19:09:54 +0800

    feat: add response package

---
+++ pkg/response/code/code.go
@@ -0,0 +1,31 @@
+package code
+
+type Code int
+
+const (
+	Success             Code = 200
+	Unauthorized        Code = 401
+	Forbidden           Code = 403
+	NotFound            Code = 404
+	InternalServerError Code = 500
+	Custom              Code = -621
+)
+
+var statusMessages = map[Code]string{
+	Success:             "OK",
+	Unauthorized:        "Unauthorized",
+	Forbidden:           "Forbidden",
+	NotFound:            "Not Found",
+	InternalServerError: "Internal Server Error",
+}
+
+func (c Code) Value() int {
+	return int(c)
+}
+
+func (c Code) Message() string {
+	if msg, exists := statusMessages[c]; exists {
+		return msg
+	}
+	return "Unknown Error"
+}
